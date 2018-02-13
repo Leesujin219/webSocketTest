99156f00fd349f395c9fd5dc921cd1a9e682d31b
leesujin219 <lee_sujin219@naver.com>
2018-02-13 17:58:42 +0900

    20180213

---
+++ src/WebSocketServer.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+
+@ServerEndpoint("/weball")
+public class WebSocketServer {
+	private static Set<Session> clients=Collections.synchronizedSet(new HashSet<Session>());
+	//ServerEndpoint 꼭 넣어야함..! 그래야 톰캣이 인식함
+	
+	@OnMessage
+	public void onMessage(String message,Session session) throws IOException{
+			System.out.println(message);
+			synchronized(clients) {
+				String id=null;
+				if(message.indexOf(":[")>0) {
+					id=message.substring(message.indexOf(":[")+2,message.indexOf("]"));
+					System.out.println("id:["+id+"]");
+				}
+				String movemessage=session.getId()+":"+message;
+				for(Session client:clients) {
+					
+					if(!client.equals(session)) {
+						
+						if(id==null) {
+							client.getBasicRemote().sendText(movemessage);
+						}
+						else if(id.equals(client.getId())) {
+							client.getBasicRemote().sendText(movemessage);
+		
+						}
+					}
+				}
+			}
+	}
+	@OnOpen
+	public void onOpen(Session session) {
+		clients.add(session);
+	}
+	@OnClose
+	public void onClose(Session session) {
+		clients.remove(session);
+	}
+	
+}
